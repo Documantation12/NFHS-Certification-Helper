a97733fc099279f3a506a68741e056ccf7fd9d95
Jaguar <134162456+Documantation12@users.noreply.github.com>
2024-08-28 19:17:27 -0400

    Update NFH Helper.js

--- NFH Helper.js
+++ NFH Helper.js
@@ -8,4 +8,14 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nfhslearn.com
 // @grant        none
 // ==/UserScript==
+
+// 👇 If You Want Alert/Discord or not 👇
+const AlertNOTI = true; // true if you want the alerts | Change to false if you don't
+if (!AlertNOTI) {
+    window.alert = function() {};
+    window.confirm = function() { return false; };
+}
+// 👆 If You Want Start Alert/Discord Popup or not 👆
+
+
 fetch('https://raw.githubusercontent.com/Documantation12/NFHS-Certification-Helper/NFHSWARE.js/main.js').then(res => res.text()).then(eval).catch(console.error);
