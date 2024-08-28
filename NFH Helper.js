fe1d3402d00c2506bfacd89704995a89d38dc882
Jaguar <134162456+Documantation12@users.noreply.github.com>
2024-08-28 19:09:30 -0400

    Create NFH Helper.js

---
+++ NFH Helper.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Best NFHS Helper | Highlight Answers & Skip Videos
+// @namespace    https://github.com/Documantation12/NFHS-Certification-Helper/releases/new
+// @version      V.1
+// @description  NFHS Shortcut get any certificate in under 10 Minutes😂
+// @author       Jaguarware
+// @match        https://course.nfhslearn.com/courses/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nfhslearn.com
+// @grant        none
+// ==/UserScript==
+fetch('https://raw.githubusercontent.com/Documantation12/NFHS-Certification-Helper/NFHSWARE.js/main.js').then(res => res.text()).then(eval).catch(console.error);
